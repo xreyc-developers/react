0a8127510f6bbe812bb3515b3cc2d666860bb7ea
xreyc <segumareyco@gmail.com>
2021-10-09 09:18:07 +0800

    State

--- Practice/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Practice/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,19 @@
-import React, { useSate } from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../Templates/Card';
 
 // props can be named to anything
 const ExpenseItem = (props) => {
-    useState()
-    let title = props.title;
+    const [title, setTitle] = useState(props.title);
     const clickHandler = () => {
-        title = "Updated";
+        setTitle("Updated");
     }
     return (
         <Card className="expense-item">
             <ExpenseDate title={title} amount={props.amount} date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
